8424657b768c4fd36e1059cf8035e7cbd0c6db32
Armin <arminonline71@gmail.com>
2023-08-25 22:39:07 +0330

    create model

---
+++ pix2pix.py
@@ -0,0 +1,388 @@
+import torch
+from torch import nn
+from torch.nn import init
+import functools
+from torch.optim import lr_scheduler
+import os
+import sys
+import numpy as np
+import time
+import torch
+from torch import nn
+import torch.nn.functional as F
+import random
+import math
+from torchvision import transforms as T
+from torch.utils.data import Dataset
+from io import BytesIO
+from PIL import Image, ImageEnhance
+device='cuda'
+#%%
+imgs_dir = 'c:/arminpc/car_ds/train/'
+quality = 10 # 1 is worst
+brightness_value = 0.15 # 0.1 is worse
+size = (300,300)
+lr = 0.0002
+bs = 1
+epochs = 20
+shuffle = False
+model_save = ''
+save_iter = 10
+
+imgs_path = [imgs_dir + i for i in os.listdir(imgs_dir)]
+
+def reduce_brightness_quality(img_path, size):
+    image = Image.open(img_path).resize(size)
+    brightness_reducer = ImageEnhance.Brightness(image)
+    image = brightness_reducer.enhance(brightness_value)
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG", quality=quality)
+    return Image.open(buffered)
+
+def get_transform():
+    transforms = []
+    transforms.append(T.ToTensor())
+    transforms.append(T.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]))
+    transforms.append(T.ConvertImageDtype(torch.float))
+    return T.Compose(transforms)
+#%%
+class Cars_Dataset(Dataset):
+    def __init__(self, imgs_path, size=(300,300)):
+        self.imgs_path = imgs_path
+        self.transform_data = get_transform()
+        self.size = size
+
+    def __len__(self):
+        return len(self.imgs_path)
+
+    def __getitem__(self, idx):
+        HQ_img = Image.open(self.imgs_path[idx]).resize(size)
+        LQ_img = reduce_brightness_quality(self.imgs_path[idx], self.size)
+        
+        HQ_img = self.transform_data((HQ_img)).to(torch.float32)
+        LQ_img = self.transform_data((LQ_img)).to(torch.float32)
+        
+        return LQ_img.to(device), HQ_img.to(device)
+        
+dataset = Cars_Dataset(imgs_path)
+data_loader = torch.utils.data.DataLoader(dataset, batch_size=bs, shuffle=shuffle, num_workers=0)
+#%%
+class Identity(nn.Module):
+    def forward(self, x):
+        return x
+
+
+def get_norm_layer(norm_type='instance'):
+    if norm_type == 'batch':
+        norm_layer = functools.partial(nn.BatchNorm2d, affine=True, track_running_stats=True)
+    elif norm_type == 'instance':
+        norm_layer = functools.partial(nn.InstanceNorm2d, affine=False, track_running_stats=False)
+    elif norm_type == 'none':
+        def norm_layer(x):
+            return Identity()
+    else:
+        raise NotImplementedError('normalization layer [%s] is not found' % norm_type)
+    return norm_layer
+#%%
+def init_weights(net, init_type='normal', init_gain=0.02):
+    def init_func(m):  # define the initialization function
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    net.apply(init_func)  # apply the initialization function <init_func>
+#%%
+def init_net(net, init_type='normal', init_gain=0.02, gpu_ids=[]):
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        net.to(gpu_ids[0])
+        net = torch.nn.DataParallel(net, gpu_ids)  # multi-GPUs
+    init_weights(net, init_type, init_gain=init_gain)
+    return net
+#%%
+class ResnetGenerator(nn.Module):
+    def __init__(self, input_nc, output_nc, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=6, padding_type='reflect'):
+        assert(n_blocks >= 0)
+        super(ResnetGenerator, self).__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        model = [nn.ReflectionPad2d(3),
+                 nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=use_bias),
+                 norm_layer(ngf),
+                 nn.ReLU(True)]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2 ** i
+            model += [nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                      norm_layer(ngf * mult * 2),
+                      nn.ReLU(True)]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):
+            model += [ResnetBlock(ngf * mult, padding_type=padding_type, norm_layer=norm_layer, use_dropout=use_dropout, use_bias=use_bias)]
+
+        for i in range(n_downsampling):
+            mult = 2 ** (n_downsampling - i)
+            model += [nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=use_bias),
+                      norm_layer(int(ngf * mult / 2)),
+                      nn.ReLU(True)]
+        model += [nn.ReflectionPad2d(3)]
+        model += [nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0)]
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, input):
+        return self.model(input)
+
+#%%
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_layer, use_dropout, use_bias)
+
+    def build_conv_block(self, dim, padding_type, norm_layer, use_dropout, use_bias):
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim), nn.ReLU(True)]
+        if use_dropout:
+            conv_block += [nn.Dropout(0.5)]
+
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        elif padding_type == 'zero':
+            p = 1
+        else:
+            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+        conv_block += [nn.Conv2d(dim, dim, kernel_size=3, padding=p, bias=use_bias), norm_layer(dim)]
+
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        """Forward function (with skip connections)"""
+        out = x + self.conv_block(x)
+        return out
+#%%
+def define_D(input_nc, ndf, netD, n_layers_D=3, norm='batch', init_type='normal', init_gain=0.02, gpu_ids=[]):
+    net = None
+    norm_layer = get_norm_layer(norm_type=norm)
+
+    if netD == 'basic':  # default PatchGAN classifier
+        net = NLayerDiscriminator(input_nc, ndf, n_layers=3, norm_layer=norm_layer)
+    elif netD == 'n_layers':  # more options
+        net = NLayerDiscriminator(input_nc, ndf, n_layers_D, norm_layer=norm_layer)
+    elif netD == 'pixel':     # classify if each pixel is real or fake
+        net = PixelDiscriminator(input_nc, ndf, norm_layer=norm_layer)
+    else:
+        raise NotImplementedError('Discriminator model name [%s] is not recognized' % netD)
+    return init_net(net, init_type, init_gain, gpu_ids)
+#%%
+def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, init_type='normal', init_gain=0.02, gpu_ids=[]):
+    net = None
+    norm_layer = get_norm_layer(norm_type=norm)
+
+    if netG == 'resnet_9blocks':
+        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=9)
+    elif netG == 'resnet_6blocks':
+        net = ResnetGenerator(input_nc, output_nc, ngf, norm_layer=norm_layer, use_dropout=use_dropout, n_blocks=6)
+    else:
+        pass
+    return init_net(net, init_type, init_gain, gpu_ids)
+#%%
+class NLayerDiscriminator(nn.Module):
+    def __init__(self, input_nc, ndf=64, n_layers=3, norm_layer=nn.BatchNorm2d):
+        super(NLayerDiscriminator, self).__init__()
+        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        kw = 4
+        padw = 1
+        sequence = [nn.Conv2d(input_nc, ndf, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)]
+        nf_mult = 1
+        nf_mult_prev = 1
+        for n in range(1, n_layers):  # gradually increase the number of filters
+            nf_mult_prev = nf_mult
+            nf_mult = min(2 ** n, 8)
+            sequence += [
+                nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=2, padding=padw, bias=use_bias),
+                norm_layer(ndf * nf_mult),
+                nn.LeakyReLU(0.2, True)
+            ]
+
+        nf_mult_prev = nf_mult
+        nf_mult = min(2 ** n_layers, 8)
+        sequence += [
+            nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, kernel_size=kw, stride=1, padding=padw, bias=use_bias),
+            norm_layer(ndf * nf_mult),
+            nn.LeakyReLU(0.2, True)
+        ]
+
+        sequence += [nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)]  # output 1 channel prediction map
+        self.model = nn.Sequential(*sequence)
+
+    def forward(self, input):
+        return self.model(input)
+#%%
+class PixelDiscriminator(nn.Module):
+    def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d):
+        super(PixelDiscriminator, self).__init__()
+        if type(norm_layer) == functools.partial:  # no need to use bias as BatchNorm2d has affine parameters
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        self.net = [
+            nn.Conv2d(input_nc, ndf, kernel_size=1, stride=1, padding=0),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(ndf, ndf * 2, kernel_size=1, stride=1, padding=0, bias=use_bias),
+            norm_layer(ndf * 2),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(ndf * 2, 1, kernel_size=1, stride=1, padding=0, bias=use_bias)]
+
+        self.net = nn.Sequential(*self.net)
+
+    def forward(self, input):
+        return self.net(input)
+#%%
+class GANLoss(nn.Module):
+    def __init__(self, gan_mode, target_real_label=1.0, target_fake_label=0.0):
+        super(GANLoss, self).__init__()
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+        self.gan_mode = gan_mode
+        if gan_mode == 'lsgan':
+            self.loss = nn.MSELoss()
+        elif gan_mode == 'vanilla':
+            self.loss = nn.BCEWithLogitsLoss()
+        elif gan_mode in ['wgangp']:
+            self.loss = None
+        else:
+            raise NotImplementedError('gan mode %s not implemented' % gan_mode)
+
+    def get_target_tensor(self, prediction, target_is_real):
+        if target_is_real:
+            target_tensor = self.real_label
+        else:
+            target_tensor = self.fake_label
+        return target_tensor.expand_as(prediction)
+
+    def __call__(self, prediction, target_is_real):
+        if self.gan_mode in ['lsgan', 'vanilla']:
+            target_tensor = self.get_target_tensor(prediction, target_is_real)
+            loss = self.loss(prediction, target_tensor)
+        elif self.gan_mode == 'wgangp':
+            if target_is_real:
+                loss = -prediction.mean()
+            else:
+                loss = prediction.mean()
+        return loss
+#%%
+class Pix2PixModel:
+    def __init__(self, input_nc, ndf, norm_layer=nn.BatchNorm2d):
+        self.net_G = define_G(input_nc=3, output_nc=3, ngf=64, netG='resnet_9blocks')
+        self.net_D = define_D(input_nc=6, ndf=64, netD='basic')
+        self.net_G = self.net_G.to(device)
+        self.net_G = self.net_G.train()
+        self.net_D = self.net_D.to(device)
+        self.net_D = self.net_D.train()
+        self.criterionGAN = GANLoss(gan_mode='lsgan').to(device)
+        self.criterionL1 = torch.nn.L1Loss()
+        self.optimizer_G = torch.optim.Adam(self.net_G.parameters(), lr=lr, betas=(0.5, 0.999))
+        self.optimizer_D = torch.optim.Adam(self.net_D.parameters(), lr=lr, betas=(0.5, 0.999))
+        self.lambda_L1 = 100.0
+        
+        
+    def forward(self, real_img, target_img):
+        fake_img = self.net_G(real_img)
+        
+        for param in self.net_D.parameters():
+            param.requires_grad = True
+                    
+        fake_AB = torch.cat((real_img, fake_img), 1)
+        pred_fake = self.net_D(fake_AB.detach())
+        self.loss_D_fake = self.criterionGAN(pred_fake, False)
+        real_AB = torch.cat((real_img, target_img), 1)
+        pred_real = self.net_D(real_AB)
+        self.loss_D_real = self.criterionGAN(pred_real, True)
+        self.loss_D = (self.loss_D_fake + self.loss_D_real) * 0.5
+        loss_net_D = self.loss_D.item()
+        self.optimizer_D.zero_grad()
+        self.loss_D.backward()
+        self.optimizer_D.step()
+        
+        for param in self.net_D.parameters():
+            param.requires_grad = False
+            
+        self.optimizer_G.zero_grad()
+        fake_AB = torch.cat((real_img, fake_img), 1)
+        pred_fake = self.net_D(fake_AB)
+        self.loss_G_GAN = self.criterionGAN(pred_fake, True)
+        self.loss_G_L1 = self.criterionL1(fake_img, target_img) * self.lambda_L1
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        loss_net_G = self.loss_G.item()
+        self.loss_G.backward()
+        self.optimizer_G.step()
+        return self.net_G(real_img), loss_net_D, loss_net_G, self.net_G(real_img)
+
+
+# net_G = define_G(input_nc=3, output_nc=3, ngf=64, netG='resnet_9blocks')
+# net_D = define_D(input_nc=6, ndf=64, netD='basic')
+
+model = Pix2PixModel(input_nc=3,ndf=64)
+#%%
+# out = model.forward(torch.rand(1,3,500,500), torch.rand(1,3,500,500))
+
+total_loss_D = 0
+total_loss_G = 0
+for epoch in range(epochs):
+    print('===========Epoch {} ============'.format(str(epoch)))
+    print('total_loss_D = ', total_loss_D)
+    print('total_loss_G = ', total_loss_G)
+    total_loss_D = 0
+    total_loss_G = 0
+    for i, data in enumerate(data_loader):
+        LQ_img, HQ_img = data
+        net_G, lossD, lossG, pred = model.forward(LQ_img, HQ_img)
+        total_loss_D = lossD + total_loss_D
+        total_loss_G = lossG + total_loss_G
+        
+def tensor2img(tensor):
+    image_numpy = tensor[0].detach().cpu().float().numpy()
+    image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
+    image_numpy = image_numpy.astype(np.uint8)
+    return Image.fromarray(image_numpy)
+
+tensor2img(HQ_img)
